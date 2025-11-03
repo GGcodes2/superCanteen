b7df25c46c48d82a1882a02de24d7c743c118e91
Gaurav Katare <gauravkatare@Gauravs-MacBook-Air.local>
2025-11-03 20:55:42 +0530

    main

--- admin-frontend/src/api/axios.js
+++ admin-frontend/src/api/axios.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "https://supercanteen-backend.onrender.com/api",
-});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) req.headers.Authorization = `Bearer ${token}`;
   return req;
+});
+
+
+const API = axios.create({
+  baseURL: "https://supercanteen-backend.onrender.com/api",
+  withCredentials: true,
 });
 
 export default API;
