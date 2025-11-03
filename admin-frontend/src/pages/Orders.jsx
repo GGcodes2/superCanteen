104f4cba5e80e68edfa5b47edca30658f1f163d2
Gaurav Katare <gauravkatare@Gauravs-MacBook-Air.local>
2025-11-03 21:19:14 +0530

    deploy

--- admin-frontend/src/pages/Orders.jsx
+++ admin-frontend/src/pages/Orders.jsx
@@ -7,8 +7,8 @@ import { io } from "socket.io-client";
 const SOCKET_URL =
   "https://supercanteen-backend.onrender.com";
 
-const socket = io(SOCKET_URL, {
-  transports: ["websocket"], // 👈 ensure reliable connection (no polling)
+const socket = io("https://supercanteen-backend.onrender.com", {
+  transports: ["websocket"],
   withCredentials: true,
 });
 
