3124ccdd7c37e5129ba2c7677ef4b2ce1404159e
Gaurav Katare <gauravkatare@Gauravs-MacBook-Air.local>
2025-11-04 00:22:26 +0530

    student

--- student-frontend/src/pages/Menu.jsx
+++ student-frontend/src/pages/Menu.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import {AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import API from "../api/axios";
 import { io } from "socket.io-client";
 
